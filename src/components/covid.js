b4fc07d34b6c55dfc0e5d65080d22fce906002d9
anujgupta25 <anujg252000@gmail.com>
2021-05-14 19:29:02 +0530

    first-commit

---
+++ src/components/covid.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+
+const Covid = ()=> {
+    const [covidData, setCovidData] = useState([]);
+
+    const api = async () => {
+        const data = await fetch('https://api.covid19india.org/data.json');
+        const res = await data.json();
+        console.log(res.statewise[20]);
+        setCovidData(res.statewise[20]);
+    }
+
+    useEffect(()=>{
+        api();
+    },[])
+    return (
+            <div className='container pt-3'>
+                <h3 className='text-center'><span className='text-danger'>Live</span> <span className='text-warning'>Covid-19 Tracker</span>  </h3>    
+                <div className='row d-flex justify-content-betwen mx-2 mt-5'>
+                <div className='col-md-3 m-md-4 mt-4 card card4'>
+                    <p>State</p>
+                    <span className='data'>{covidData.state}</span>
+                </div>
+
+                <div className='col-md-3 m-md-4 mt-4 card card1'>
+                    <p>Active Cases</p>
+                    <span className='data'>{covidData.active}</span>
+                </div>
+
+                <div className='col-md-3 m-md-4 mt-4 card card2'>
+                    <p>Confirmed Cases</p>
+                    <span className='data'>{covidData.confirmed}</span>
+                </div>
+
+                <div className='col-md-3 m-md-4 mt-4 card card3'>
+                    <p>Total Deaths</p>
+                    <span className='data'>{covidData.deaths}</span>
+                </div>
+
+                <div className='col-md-3 m-md-4 mt-4 card card5'>
+                    <p>Total Recovered</p>
+                    <span className='data'>{covidData.recovered}</span>
+                </div>
+                <div className='col-md-3 m-md-4 mt-4 card card6'>
+                    <p>Last Updated at</p>
+                    <span className='data'>{covidData.lastupdatedtime}</span>
+                </div>
+                <div className='col m-md-4 mt-4 card card7'>
+                    <p>State Note</p>
+                    <span className='data'>{covidData.statenotes}</span>
+                </div>
+                
+
+                
+
+            </div>                
+            </div>
+    )
+}
+export default Covid;
